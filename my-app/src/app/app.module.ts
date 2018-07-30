62596fb9ff0033db2608abc77d5dfdef1826e611
Yuzhu JIN <yjin@rs2i.fr>
2018-07-30 15:39:35 +0200

    install ng-bootstrap using ng add

--- my-app/src/app/app.module.ts
+++ my-app/src/app/app.module.ts
@@ -3,9 +3,12 @@ import { NgModule } from '@angular/core';
 
 //module a-z
 import { CoreModule } from './core/core.module';
+import { HomeModule } from './home/home.module';
+import { ItemsModule } from './items/items.module';
 import { LoginModule } from './login/login.module';
 import { PageNotFoundModule } from './page-not-found/page-not-found.module';
 import { SharedModule } from './shared/shared.module';
+import { NgbModule } from '@ng-bootstrap/ng-bootstrap';
 
 //component a-z
 import { AppComponent } from './app.component';
@@ -19,7 +22,10 @@ import { AppComponent } from './app.component';
     CoreModule,
     LoginModule,
     PageNotFoundModule,
-    SharedModule
+    SharedModule,
+    HomeModule,
+    ItemsModule,
+    NgbModule.forRoot()
   ],
   providers: [],
   bootstrap: [AppComponent]
