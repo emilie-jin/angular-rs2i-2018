cbcad4b49973ee67488c5f8663d1fdd6312f3685
Yuzhu JIN <yjin@rs2i.fr>
2018-07-31 17:40:48 +0200

    ng bootstrap collapse

--- my-app/src/app/shared/shared.module.ts
+++ my-app/src/app/shared/shared.module.ts
@@ -4,13 +4,14 @@ import { NavComponent } from './components/nav/nav.component';
 import { ItemComponent } from './components/item/item.component';
 import { CapitalizePipe } from './pipes/capitalize.pipe';
 import { FilterByStatePipe } from './pipes/filter-by-state.pipe';
+import {NgbModule} from '@ng-bootstrap/ng-bootstrap';
 import { StateDirective } from './directives/state.directive';
 
 @NgModule({
   imports: [
-    CommonModule
+    CommonModule,
+    NgbModule
   ],
   declarations: [NavComponent, ItemComponent, CapitalizePipe, FilterByStatePipe,StateDirective],
-  exports: [NavComponent,ItemComponent,FilterByStatePipe]
-})
+  exports: [NavComponent,ItemComponent,FilterByStatePipe],})
 export class SharedModule { }
